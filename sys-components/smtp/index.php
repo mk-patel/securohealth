e954b8d4b0f13d4d6c6b548b8bf5c5e65cd8c63f
Manish Patel <61538824+mk-patel@users.noreply.github.com>
2022-03-14 14:53:19 +0530

    Add files via upload

---
+++ sys-components/smtp/index.php
@@ -0,0 +1,34 @@
+<?php
+
+// Including PHPMiler to send otp.
+include('smtp/PHPMailerAutoload.php');
+
+// Function to send the mail.
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	//$mail->SMTPDebug  = 3;
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->Username = "mkp.manishpatel@gmail.com";
+	$mail->Password = "";
+	$mail->SetFrom("mkp.manishpatel@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
+?>
