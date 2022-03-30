59626253c09bf3c71b1142a628d13c68f1d07dee
Manish Patel <61538824+mk-patel@users.noreply.github.com>
2022-03-30 18:51:22 +0530

    Add files via upload

--- securohealth_2799.sql
+++ securohealth_2799.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2022 at 12:26 PM
+-- Generation Time: Mar 30, 2022 at 03:18 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -21,6 +21,24 @@ SET time_zone = "+00:00";
 --
 -- Database: `securohealth_2799`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing`
+--
+
+CREATE TABLE `billing` (
+  `bill_id` int(10) NOT NULL,
+  `bill_order_id` varchar(200) NOT NULL,
+  `bill_txn_id` varchar(500) NOT NULL,
+  `bill_amount` varchar(500) NOT NULL,
+  `bill_trt_id` int(20) NOT NULL,
+  `bill_user_id` int(20) NOT NULL,
+  `bill_hp_id` int(10) NOT NULL,
+  `bill_date` varchar(200) NOT NULL,
+  `bill_status` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -43,6 +61,8 @@ CREATE TABLE `blocked_hospital` (
 
 CREATE TABLE `hospital` (
   `hp_id` int(20) NOT NULL,
+  `hp_email` varchar(200) NOT NULL,
+  `hp_password` varchar(500) NOT NULL,
   `hp_name` mediumtext NOT NULL,
   `hp_type` varchar(500) NOT NULL,
   `hp_speciality` mediumtext NOT NULL,
@@ -52,15 +72,12 @@ CREATE TABLE `hospital` (
   `hp_district` varchar(200) NOT NULL,
   `hp_city` varchar(200) NOT NULL,
   `hp_pincode` int(100) NOT NULL,
-  `hp_desc` mediumtext NOT NULL
+  `hp_desc` mediumtext NOT NULL,
+  `hp_files` varchar(500) NOT NULL,
+  `hp_date` varchar(200) NOT NULL,
+  `hp_time` varchar(200) NOT NULL,
+  `hp_status` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `hospital`
---
-
-INSERT INTO `hospital` (`hp_id`, `hp_name`, `hp_type`, `hp_speciality`, `hp_license`, `hp_address`, `hp_state`, `hp_district`, `hp_city`, `hp_pincode`, `hp_desc`) VALUES
-(1, 'fry', '1', 'd', '1', 'c', '1', '1', '1', 3333, 'x');
 
 -- --------------------------------------------------------
 
@@ -80,18 +97,6 @@ CREATE TABLE `treatment_record` (
   `tr_date` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `treatment_record`
---
-
-INSERT INTO `treatment_record` (`tr_id`, `tr_trt_id`, `tr_title`, `tr_desc`, `tr_cost`, `tr_files`, `tr_doctor`, `tr_type`, `tr_date`) VALUES
-(1, 2, 'ttttt', '667', '400', '0', 'ghuu', 1, ''),
-(2, 2, 'NA', '  rr', '0', '', 'dd', 1, ''),
-(3, 2, 'NA', '  rr', '0', '', 'dd', 1, '2022-03-19, 06:51 PM'),
-(4, 2, 'NA', '  dd', '0', '', 'ff', 1, '2022-03-19, 06:51 PM'),
-(5, 2, 'WJ4=', 'Nv/7CQ==', 'Jg==', '', 'fbQ=', 1, 'JO/4Cm2f0oyL72WfSpMuVKQTd88='),
-(6, 2, 'WJ4=', 'Nv+uXA==', 'Jg==', 'WJ4=', 'c7o=', 1, 'JO/4Cm2f0oyL72WfSpMuVacTd88=');
-
 -- --------------------------------------------------------
 
 --
@@ -109,16 +114,9 @@ CREATE TABLE `treatment_session` (
   `trt_closing_date` varchar(200) NOT NULL,
   `trt_user_id` int(10) NOT NULL,
   `trt_hp_id` int(10) NOT NULL,
-  `trt_completed` int(1) NOT NULL
+  `trt_completed` int(1) NOT NULL,
+  `trt_ref_id` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `treatment_session`
---
-
-INSERT INTO `treatment_session` (`trt_id`, `trt_name`, `trt_inf`, `trt_dis`, `trt_srt`, `trt_corm`, `trt_date`, `trt_closing_date`, `trt_user_id`, `trt_hp_id`, `trt_completed`) VALUES
-(1, 'abcd', '1', '1', '1', '1', 'f4g', 'f4g', 1, 1, 0),
-(2, 'ttt', 'VL6pTCXdiMDW', 'VbCmXA==', 'WrC9', 'WLA=', 't4', 't4', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -150,15 +148,14 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `user_first_name`, `user_last_name`, `user_email`, `password`, `user_gender`, `user_dob`, `user_address`, `user_city`, `user_status`, `user_pincode`, `user_otp`, `user_verified`, `user_image`, `user_qrcode`, `user_card`, `user_card_status`, `user_date`, `user_time`, `user_govt`) VALUES
-(1, 'W76kUTPH', 'Rr6+XSw=', 'e76kUTPHitGJ73D/Hcl1Df8dRO23', 'c514e10ab5bcfa0dcac130d2277d4ce344d2390a', 'e76mXQ==', 'JO/6Cm2f0IyK4g==', 'Q6+rSmD/gNPb+mnxH8VmRMBQT+21kA==', 'XberUTLFicjOv2nUG8h1Cg==', '1', 'Iub7C3OX', 361158, 1, 'f7KrXyXczpLZ5XndTZEkVPZQFrrq0tFHVs1v+71GUC4ZQ0o=', 'Z62pVyTKko6L+DnRHQ==', 'Iej9D3iY05eO73k=', 0, '2002-01-01', '12:36 AM', 1);
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`bill_id`);
 
 --
 -- Indexes for table `blocked_hospital`
@@ -195,6 +192,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `bill_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `blocked_hospital`
 --
 ALTER TABLE `blocked_hospital`
@@ -204,25 +207,25 @@ ALTER TABLE `blocked_hospital`
 -- AUTO_INCREMENT for table `hospital`
 --
 ALTER TABLE `hospital`
-  MODIFY `hp_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `hp_id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `treatment_record`
 --
 ALTER TABLE `treatment_record`
-  MODIFY `tr_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `tr_id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `treatment_session`
 --
 ALTER TABLE `treatment_session`
-  MODIFY `trt_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `trt_id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
